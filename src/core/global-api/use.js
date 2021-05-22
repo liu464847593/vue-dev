f041d657374ebd99f6f379908141a1b6267dffb7
liuzhijian <18576746147@163.com>
2021-05-22 20:54:42 +0800

    doc:  vue.use函数注解

--- src/core/global-api/use.js
+++ src/core/global-api/use.js
@@ -5,13 +5,13 @@ import { toArray } from '../util/index'
 export function initUse (Vue: GlobalAPI) {
   Vue.use = function (plugin: Function | Object) {
     const installedPlugins = (this._installedPlugins || (this._installedPlugins = []))
-    if (installedPlugins.indexOf(plugin) > -1) {
+    if (installedPlugins.indexOf(plugin) > -1) { // 判断是否被注册，如果注册过则终止
       return this
     }
 
     // additional parameters
-    const args = toArray(arguments, 1)
-    args.unshift(this)
+    const args = toArray(arguments, 1) // 除第一个参数外，剩余所有参数将得到的列表赋值到args中
+    args.unshift(this) // 把vue添加到最前面，包装install方法第一个参数是vue
     if (typeof plugin.install === 'function') {
       plugin.install.apply(plugin, args)
     } else if (typeof plugin === 'function') {
